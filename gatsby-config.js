19a768cfb8ea12831e2cf2813f391917d1a33516
Frankie Flood <fsfloody@gmail.com>
2021-02-17 11:12:06 +0000

    install gatsby-source-filesystem & begin to render images

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,14 @@ module.exports = {
     title: "Shop CMS",
     author: "Frankie",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
